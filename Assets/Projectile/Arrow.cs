dafa77d22582f72f7227225e01b5d8fbde5587b8
Lithius0 <aerosiiap@gmail.com>
2022-10-02 17:37:14 -0400

    Added new Arrow texture

--- Assets/Projectile/Arrow.cs
+++ Assets/Projectile/Arrow.cs
@@ -6,7 +6,7 @@ public class Arrow : MonoBehaviour
 {
 
     [SerializeField] public float penetration = 20;
-    [SerializeField] public float expirationTime = 5;
+    [SerializeField] public float expirationTime = 3;
 
     private Vector3 velocity;
     private bool inFlight = false;
@@ -35,6 +35,7 @@ public class Arrow : MonoBehaviour
                     hit.collider.gameObject.GetComponent<ChetSceneManager>().OnArrowHit();
                 }
 
+
                 //hit.collider should always return true since raycasts scans for colliders.
                 Armor armorHit = hit.collider.gameObject.GetComponent<Armor>();
 
@@ -43,17 +44,25 @@ public class Arrow : MonoBehaviour
                     float effectiveThickness = armorHit.GetThickness(hit.point, velocity);
                     if (effectiveThickness > this.penetration)
                     {
-                        //Reflection
+                        Debug.Log("Reflected, thickness: " + effectiveThickness + "; penetration: " + penetration);
+
+                        Reflect(hit.normal);
                         nextPosition = hit.point;
-                        //https://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
-                        this.velocity = this.velocity - (2 * Vector3.Dot(this.velocity, hit.normal) * hit.normal);
+                        //Offset the next off the armor plate a tiny bit to prevent raycast hitting the same collider twice.
+                        nextPosition += (hit.normal * 0.01f);
                     }
 
                 }
                 else
                 {
+                    Debug.Log("Reflected");
+
                     //If the collider hit isn't an armor plate, just reflect
-                    this.velocity = this.velocity - (2 * Vector3.Dot(this.velocity, hit.normal) * hit.normal);
+                    Reflect(hit.normal);
+
+                    nextPosition = hit.point;
+                    //Offset the next off the armor plate a tiny bit to prevent raycast hitting the same collider twice.
+                    nextPosition += (hit.normal * 0.01f);
                 }
 
             }
@@ -75,6 +84,12 @@ public class Arrow : MonoBehaviour
     {
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 180;
         gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    protected void Reflect(Vector3 normal)
+    {
+        //https://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
+        velocity -= (2 * Vector3.Dot(this.velocity, normal) * normal);
     }
 
     public void Launch(Vector3 velocity)
